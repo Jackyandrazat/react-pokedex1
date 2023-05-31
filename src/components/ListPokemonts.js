dea6bf78d13af634795a81756f04ffe16754b1a1
Jacky Andrazat <82565381+Jackyandrazat@users.noreply.github.com>
2023-05-31 10:34:04 +0700

    Update ListPokemonts.js
    update add pokemon berdasar api

--- src/components/ListPokemonts.js
+++ src/components/ListPokemonts.js
@@ -1,54 +1,63 @@
-/* eslint-disable no-global-assign */
-import React, { useEffect,useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-  
-  function addPokemon(pokemon) {
-    // Ambil data dari local storage (jika ada)
-    const existingData = localStorage.getItem("myPokemon");
-  
-    // Jika data tidak ditemukan, buat array kosong
-    const myPokemon = existingData ? JSON.parse(existingData) : [];
-  
-    const isDataExist = myPokemon.some((p) => p.name === pokemon.name);
-  
-    if (isDataExist) {
-      alert("Pokemon sudah ada!");
-    } else {
-      // Tambahkan pokemon yang dipilih ke dalam array myPokemon
-      myPokemon.push(pokemon);
-      // Simpan data ke local storage
-      localStorage.setItem("myPokemon", JSON.stringify(myPokemon));
-      // Tampilkan pesan sukses
-      alert("Pokemon berhasil ditambahkan ke koleksi kamu!");
-    }
-  }
-  
-  
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 export default function ListPokemonts() {
   const [pokemons, setPokemons] = useState([]);
 
-
   const fetchPokemons = async () => {
     try {
       const response = await axios.get('http://localhost:4000/pokemons');
-      console.log("response" , response.data);
+      console.log('response', response.data);
       const pokemonList = response.data.data;
       setPokemons(pokemonList);
     } catch (error) {
       console.log(error);
     }
-  }
+  };
+
+  const addPokemon = async (item) => {
+    try {
+      const getLocalStorageIdUser = localStorage.getItem('idUserLogged');
+      const data = {
+        id_pokemons: item.id,
+        id_user: getLocalStorageIdUser,
+      };
+  
+      const existingDataDb = await axios.get('http://localhost:4000/pokemons/collection', {
+        params: {
+          id_pokemons: item.id,
+          id_user: getLocalStorageIdUser,
+         },
+      });
+  
+      if (existingDataDb.data.data.find(data => data.pokemons_id === item.id)) {
+        toast.error("Pokemon already exists",{
+          autoClose: 200
+        })
+        return;      
+      }
+      const response = await axios.post('http://localhost:4000/pokemons/collection', data);
+      toast.success("Pokemon Added",{
+        autoClose: 1000
+      })
+      console.log('Data berhasil ditambahkan ke database:', response.data);
+    } catch (error) {
+      console.log('Gagal menambahkan data ke database:', error.message);
+    }
+  };
 
   useEffect(() => {
     fetchPokemons();
   }, []);
 
-
   return (
     <div className="">
+    <ToastContainer />
       <div className="grid mb-8 grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-        {pokemons.map((item,index) => (
+        {pokemons.map((item, index) => (
           <div key={index} className="group relative">
             <div className="aspect-h-1 aspect-w-1 w-full my-8 overflow-hidden rounded-lg drop-shadow-lg xl:aspect-h-8 xl:aspect-w-7">
               <img
@@ -57,14 +66,21 @@ export default function ListPokemonts() {
                 className="object-contain h-48 w-96 lg:h-48 lg:w-96"
               />
             </div>
-            <h3 className="mt-4 ml-2 text-lg text-base-100 font-bold">{item.name_pokemon }</h3>
+            <h3 className="mt-4 ml-2 text-lg text-base-100 font-bold">{item.name_pokemon}</h3>
             <div className="mt-5 relative">
-              <a  href={`/detail/${item.id}`} className='ml-2 text-sm text-thirdColor hover:link-hover'>See Details</a>
-              <button onClick={()=> addPokemon(item)} className="btn absolute btn-sm top-0 right-5 bg-thirdColor border-0 text-neutral-900 hover:bg-fourthColor"> + </button>
+              <a href={`/detail/${item.id}`} className="ml-2 text-sm text-thirdColor hover:link-hover">
+                See Details
+              </a>
+              <button
+                onClick={() => addPokemon(item)}
+                className="btn absolute btn-sm top-0 right-5 bg-thirdColor border-0 text-neutral-900 hover:bg-fourthColor"
+              >
+                +
+              </button>
             </div>
           </div>
         ))}
       </div>
     </div>
-  )
+  );
 }
