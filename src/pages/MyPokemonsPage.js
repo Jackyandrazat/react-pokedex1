52dfcd1be4c38615d0501aaa94821a6cefd51268
Jacky Andrazat <82565381+Jackyandrazat@users.noreply.github.com>
2023-05-31 14:33:18 +0700

    Update MyPokemonsPage.js
    add package toastify

--- src/pages/MyPokemonsPage.js
+++ src/pages/MyPokemonsPage.js
@@ -1,6 +1,8 @@
 import Footer from '../components/Footer';
 import MyPokemons from '../components/MyPokemons';
 import Navbar from '../components/Navbar';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 function MyPokemonsPage() {
@@ -8,6 +10,7 @@ function MyPokemonsPage() {
   <div className='bg-secondaryColor'>
     <Navbar />
     <MyPokemons />
+    <ToastContainer />
     <Footer />
   </div>
 
